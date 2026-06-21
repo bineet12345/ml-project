0bb3a00ce69d38ca109c5095cefd5410eae32a2f
CRAZYCHAR1234 <lenkabineet02@gmail.com>
2026-06-21 18:38:33 +0530

    data ingestion

---
+++ src/mlproject/utils.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host = os.getenv('host')
+user = os.getenv('user')
+password = os.getenv('password')
+db = os.getenv('db')
+
+def read_sql_data():
+    logging.info("Reading data from mysql database started")
+    try:
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db,
+            ssl_disabled=True  # 1. Fixes the ssl.SSLError: [ASN1: NOT_ENOUGH_DATA] bug
+        )
+        # 2. Fixes logging syntax (changed from comma to f-string format)
+        logging.info(f"Connection established: {mydb}") 
+        
+        df = pd.read_sql('Select * from students', mydb)
+        print(df.head())
+
+        return df
+    except Exception as ex:
+        # 3. Fixes the TypeError by passing 'sys' to provide error details
+        raise CustomException(ex, sys)
